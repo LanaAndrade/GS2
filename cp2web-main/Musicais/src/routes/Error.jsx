5a795128d6128d7b5731c6375dc7873463d238f5
Lana Andrade <lanaandrade2003@gmail.com>
2024-05-09 13:57:47 -0300

    testee

---
+++ cp2web-main/Musicais/src/routes/Error.jsx
@@ -0,0 +1,12 @@
+function Error() {
+  return (
+  <>
+  <main id="main-error">
+    <h1 id="error-404">Erro 404</h1>
+    <p className="error-text">Página não encontrada</p>
+    <p className="error-text">URL digitada não está disponível nos servidores do site</p>
+  </main>
+  </>
+  )
+}
+export default Error
---
+++ cp2web-main/Musicais/src/routes/Login.jsx
@@ -0,0 +1,53 @@
+import { useRef } from 'react';
+import ListaProdutos from './ListaProdutos';
+import '../css/estilo.css';
+
+function Login() {
+  /* Hook- useRef ele retorna uma referencia a um elemento ou componentsem tem que ter que ser
+  renderizado novamente*/
+  const usuario = useRef();
+  const senha = useRef();
+
+  /*pegando os dados de usuario e senha e gravando na sessão */
+  const getUsuario = sessionStorage.getItem('usuario');
+  const getSenha = sessionStorage.getItem('senha');
+
+  const handleSubmit = () => {
+    if (usuario.current.value == 'Admin' && senha.current.value == '12345') {
+      //criando um token de autenticação
+      let token =
+        Math.random().toString(16).substring(2) +
+        Math.random().toString(16).substring(2);
+
+      sessionStorage.setItem('usuario', 'Admin');
+      sessionStorage.setItem('senha', token);
+    } else {
+      alert('Usuario e senha Inválidos !!!');
+    }
+  };
+
+  return (
+    <section className="login">
+      {/*condição ternária */}
+      {getUsuario && getSenha ? (
+        <ListaProdutos/>
+      ) : (
+        <form className="frmLogin" onSubmit={handleSubmit} >
+
+            <h2 className="ttLogin">Bem-vindo faça seu login para continuar</h2>
+
+            <p className="fieldLogin">
+
+              <input type="text" className="inputLogin" placeholder="Seu Usuário" ref={usuario} />
+
+              <input type="password" className="inputLogin" placeholder="Sua senha" ref={senha} />
+
+            </p>
+            
+            <button className='btnInputLogin' type="submit">Fazer login</button>
+        </form>
+      )}
+    </section>
+  );
+}
+export default Login;
