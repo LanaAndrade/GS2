52a4a3c1f2410390954686a43d94d44edae7ecba
lanaandrade <lanaandrade2003@gmail.com>
2024-05-15 20:49:27 -0300

    fix: design

--- challenge/hqQuadrinhos/src/routes/Apresentacao.jsx
+++ challenge/hqQuadrinhos/src/routes/Apresentacao.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Apresentacao() {
 
   return (
-    <section className="pitch">
+    <section className="pitch image">
         <h2 id="padding-titulo">Separamos aqui um breve pitch para apresentar melhor nosso projeto</h2>
         <iframe id="iframe-pitch" width="700" height="395" src="https://www.youtube.com/embed/b3wKX77eU0g" title="CHALLENGE 2023/2024 - ESPS 1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    
     </section>
